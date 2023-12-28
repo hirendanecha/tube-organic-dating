121404a2e7df120ad4ef439b2ff89e7e50295037
prashant <ppo.opash@gmail.com>
2023-12-28 17:08:40 +0530

    Chnage links

--- src/app/@shared/services/seo.service.ts
+++ src/app/@shared/services/seo.service.ts
@@ -69,7 +69,7 @@ export class SeoService {
     }
 
     if (!tagValue.image) {
-      tagValue.image = 'https://freedom.buzz/assets/images/banner/freedom-buzz-high-res.jpeg';
+      tagValue.image = 'https://tube.organic.dating/assets/Healing-Tube-Logo.jpg';
     }
 
     this.meta.updateTag({
